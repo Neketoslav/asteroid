d4ba17a2621e76725937b6f338ec3a7e1829158c
Nikita <stalker6661666@gmail.com>
2021-10-04 11:54:28 +0300

    4

---
+++ Assets/Scripts/Enemy/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+internal sealed class Asteroid : Enemy
+{
+    GameObject player;
+
+    public float speed = 0.001f;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed);
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,42 +2,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class PlayerModel : MonoBehaviour
+public class Player : MonoBehaviour
 {
     internal float force = 1.0f;
     internal float acceleration = 2.0f;
-    internal float health = 3.0f;
+    internal float health = 1.0f;
     internal float forceShot = 1000.0f;
 
     internal Vector3 direction;
 
-    internal Camera _camera;
-
     internal Rigidbody2D rb;
     internal Rigidbody2D rbRocket;
 
-    internal IShot shipShot;
-    internal Ship ship;
-    internal Interactive interactive;
-
+    [SerializeField] internal Sprite sprite;
     [SerializeField] internal Transform barrel;
 
+    internal IShot shipShot;
+    internal Ship ship { get; private set; }
+
+    private Player player;
+    private Camera mainCamera;
     public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rbRocket = Resources.Load<Rigidbody2D>("Rocket");
-        _camera = Camera.main;
+        sprite = Resources.Load<Sprite>("Bullet");
+        mainCamera = Camera.main;
         var move = new Acceleration(rb, force, acceleration);
         var rotation = new RotationShip(transform);
         ship = new Ship(move, rotation);
-        shipShot = new ShipShot(rbRocket, forceShot);
-        interactive = new Interactive();
+        shipShot = new ShipShot(sprite, barrel);
     }
-    public void PlayerControl()
+
+    public void Move()
     {
-        var direction = Input.mousePosition - _camera.WorldToScreenPoint(transform.position);
+        var direction = Input.mousePosition - mainCamera.WorldToScreenPoint(transform.position);
         ship.Rotation(direction);
         ship.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             ship.AddAcceleration();
@@ -46,10 +48,23 @@ public class PlayerModel : MonoBehaviour
         {
             ship.RemoveAcceleration();
         }
-        shipShot.Shot(barrel);
+        shipShot.Shot(sprite);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        interactive.Hurt();
+
+        if (CompareTag("Enemy"))
+        {
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                health--;
+            }
+        }
+
     }
 }
